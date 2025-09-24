1f20df433dd029c13f6d02864596cea0c6e5d8b4
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-09-24 14:00:09 +0530

    feat : added more examples on having clause with query

--- aggregation/having.sql
+++ aggregation/having.sql
@@ -43,5 +43,30 @@ HAVING SUM(SAL)>3800;
 SELECT COUNT(*),deptno FROM emp
 WHERE job = 'MANAGER'
 GROUP BY DEPTNO
-HAVING COUNT(*)=2;
+HAVING COUNT(*)>=2;
 
+-- Write a query to display each job and the maximum salary in that job, but only include jobs where the maximum salary is greater than 2600.
+SELECT JOB,MAX(SAL) FROM emp
+GROUP BY JOB
+HAVING MAX(SAL)>2600;
+
+-- Write a query to find all hire dates on which more than one employee was hired.
+SELECT HIREDATE FROM emp
+GROUP BY HIREDATE
+HAVING COUNT(*)>1;
+
+-- Write a query to display each department number and the average salary of employees in that department, but only include departments where the average salary is less than 3000.
+SELECT deptno,AVG(Sal) FROM emp
+GROUP BY DEPTNO
+HAVING AVG(SAL)<3000;
+
+-- Write a query to display department numbers where at least 3 employees have names containing the letters 'A' or 'S'.
+SELECT deptno FROM emp
+WHERE ename like '%A%' or ENAME like '%S%'
+GROUP BY DEPTNO
+HAVING COUNT(*)>=3;
+
+-- Write a query to display each job along with the minimum and maximum salary for that job, but only include jobs where the minimum salary is greater than 1000 and the maximum salary is less than 5000.
+SELECT MIN(SAL),MAX(SAL),JOB FROM emp
+GROUP BY JOB
+HAVING MIN(Sal) > 1000 and MAX(SAL)< 5000;
