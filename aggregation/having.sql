76199d16bb1da7c31a710ef522bad259a4dcb739
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-09-24 13:52:52 +0530

    feat : added having clause queries with examples

--- aggregation/having.sql
+++ aggregation/having.sql
@@ -11,5 +11,37 @@
 -- 4. HAVING
 -- 5. SELECT
 
--- Write a query to display duplicate salary
+-- Write a query to display the salary and number of employees who are earning the same salary (more than one employee).
+SELECT COUNT(*),sal FROM emp
+GROUP BY SAL
+HAVING COUNT(*)>1;
+
+-- Write a query to find employee names that are repeated (same name appears more than once).
+SELECT COUNT(*),ename FROM emp
+GROUP BY ENAME
+HAVING COUNT(*)>1;
+
+-- Write a query to display department numbers and the number of clerks in each department, but only show departments having at least 2 clerks.
+SELECT COUNT(*),deptno FROM emp
+WHERE JOB IN ('CLERK')
+GROUP BY DEPTNO
+HAVING COUNT(*) >= 2;
+
+-- Write a query to display department numbers and the total salary of employees in each department, but only for departments having at least 4 employees.
+SELECT deptno,SUM(sal) FROM emp
+GROUP BY DEPTNO
+HAVING COUNT(*)>=4;
+
+
+-- Write a query to display each job title and the number of employees earning more than 1200 salary, but only include jobs where the total salary of such employees is greater than 3800.
+SELECT COUNT(*),JOB FROM emp
+WHERE sal>1200
+GROUP BY JOB
+HAVING SUM(SAL)>3800;
+
+-- Write a query to display department numbers where exactly 2 managers are working, along with the count of managers.
+SELECT COUNT(*),deptno FROM emp
+WHERE job = 'MANAGER'
+GROUP BY DEPTNO
+HAVING COUNT(*)=2;
 
