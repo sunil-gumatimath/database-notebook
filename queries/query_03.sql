406c05d683e86cc8f08ae685c2c1d1c6083ee9f2
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-09-25 22:14:57 +0530

    feat : added pre sub querys with example

--- queries/query_03.sql
+++ queries/query_03.sql
@@ -6,3 +6,32 @@ WHERE ename = 'SMITH';
 
 SELECT ename FROM emp
 WHERE EMPNO = 7902;
+
+-- Write a query to find manager name for 'ALLEN'
+
+SELECT mgr FROM emp
+WHERE ENAME = 'ALLEN';
+
+SELECT ename FROM emp
+WHERE mgr = 7698;
+
+-- Write a query to find manager name for 'WARD'
+SELECT mgr FROM emp
+WHERE ENAME = 'WARD';
+
+SELECT ename FROM emp
+WHERE mgr = 7698;
+
+-- Write a query to find manager name for 'JONES'
+SELECT mgr FROM emp
+WHERE ENAME = 'JONES';
+
+SELECT ename FROM emp
+WHERE mgr = 7839;
+
+-- Write a query to find manager for 'MARTIN'
+SELECT mgr FROM emp
+WHERE ENAME = 'MARTIN';
+
+SELECT ename FROM emp
+WHERE mgr = 7698;
