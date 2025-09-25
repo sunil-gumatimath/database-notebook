ffe89f26e649be95cd42397d74ad894beadc5c74
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-09-25 22:40:59 +0530

    feat: add manager lookup queries for employees TURNER, ADAMS, JAMES, FORD, and MILLER
    Added SQL queries to retrieve manager IDs and list subordinates for each specified employee, enhancing the subquery examples in the file.

--- queries/subquery_01.sql
+++ queries/subquery_01.sql
@@ -65,11 +65,36 @@ SELECT ename FROM emp
 WHERE mgr IS NULL;
 
 -- Write a query to find manager for 'TURNER'
+SELECT mgr FROM emp
+WHERE ENAME = 'TURNER';
+
+SELECT ename FROM emp
+WHERE mgr = 7698;
 
 -- Write a query to find manager for 'ADAMS'
+SELECT mgr FROM emp
+WHERE ENAME = 'ADAMS';
+
+SELECT ename FROM emp
+WHERE mgr = 7788;
 
 -- Write a query to find manager for 'JAMES'
+SELECT mgr FROM emp
+WHERE ENAME = 'JAMES';
+
+SELECT ename FROM emp
+WHERE mgr = 7698;
 
 -- Write a query to find manager for 'FORD'
+SELECT mgr FROM emp
+WHERE ENAME = 'FORD';
+
+SELECT ename FROM emp
+WHERE mgr = 7566;
 
 -- Write a query to find manager for 'MILLER'
+SELECT mgr FROM emp
+WHERE ENAME = 'MILLER';
+
+SELECT ename FROM emp
+WHERE mgr = 7782;
