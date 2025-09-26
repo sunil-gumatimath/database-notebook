ae98606e3076da144396da7d53d54221f2c878b7
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-09-26 18:12:07 +0530

    feat(subqueries): add new SQL queries for employee data analysis
    - Added query to find employees hired in the same month as 'JAMES'
    - Added query to identify employees earning the same salary
    - Added queries to find employees with highest and lowest salaries

--- subqueries/subquery_02.sql
+++ subqueries/subquery_02.sql
@@ -20,3 +20,35 @@ WHERE ENAME = 'MARTIN';
 
 SELECT * FROM emp
 WHERE job = 'SALESMAN';
+
+-- Write a query to display employee details where emp hired in month same as 'JAMES'
+
+SELECT MONTH(HIREDATE) FROM emp
+WHERE ENAME = 'JAMES';
+
+SELECT ename FROM emp
+WHERE MONTH(HIREDATE) = 12;
+
+-- Write a query to display where emp earning same salary
+SELECT sal FROM emp
+GROUP BY SAL
+HAVING COUNT(*) > 1;
+
+SELECT * FROM emp
+WHERE SAL IN (1250,3000);
+
+-- Write a query to display employee earning highest salary
+
+SELECT sal FROM emp
+ORDER BY sal DESC;
+
+SELECT ename FROM emp
+WHERE sal = 5000;
+
+-- Write a query to display employee earning lowest salary
+
+SELECT sal FROM emp
+ORDER BY sal ASC;
+
+SELECT ename FROM emp
+WHERE SAL = 800;
