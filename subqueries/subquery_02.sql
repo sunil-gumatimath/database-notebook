0b61222ab433cacf2f774965c3df52034cdc9af0
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-09-26 13:29:14 +0530

    feat(subqueries): Add subqueries for employee salary and job comparisons
    - Added queries to compare salaries against Allen and Ford
    - Included query to find employees with same job as Martin
    - Demonstrates subquery usage for filtering based on specific employee data

--- subqueries/subquery_02.sql
+++ subqueries/subquery_02.sql
@@ -1,3 +1,22 @@
 SELECT * FROM emp;
 
 -- Write a query to display employee details where emp earning sal more than 'ALLEN'
+SELECT sal FROM emp
+WHERE ename = 'ALLEN';
+
+SELECT * FROM emp
+WHERE SAL > 1600;
+
+-- Write a query to display who's earning sal less than FORD sal
+SELECT sal FROM emp
+WHERE ENAME = 'FORD';
+
+SELECT * FROM emp
+WHERE SAL < 3000;
+
+-- Write a query to display employee name, designation where emp working in designation same as 'MARTIN' designation
+SELECT JOB FROM emp
+WHERE ENAME = 'MARTIN';
+
+SELECT * FROM emp
+WHERE job = 'SALESMAN';
