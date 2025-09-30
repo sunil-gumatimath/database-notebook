7d7b5e5c8d2658673cadaa718f80234a133e8c0a
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-09-30 21:27:50 +0530

    feat: add initial SQL assignment queries for employee data retrieval

---
+++ subqueries/assignments/assignment_0.sql
@@ -0,0 +1,12 @@
+-- Write a query to find deot name for the employee 1st name id donald
+
+-- write a query to find country-name where the employee first name is nancy
+
+-- write a query to find regin-name where the employee first_name is julia
+
+-- write a query to find job-title where the employee firstname is elizabeth
+
+-- write a query to find max,min salary given to a desigination where employee first_name is alysaa
+
+-- write a query to find employee details where employee working in dept administration and markiting
+
