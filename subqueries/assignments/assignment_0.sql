1fdc5f230cb53197a2c553d014643bbdb53af7e4
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-10-03 23:39:36 +0530

    feat(subqueries): Add query for employees in Asia and Europe regions
    - Added a new SQL query using nested subqueries to filter employees by region, enhancing subquery practice in assignment_0.

--- subqueries/assignments/assignment_0.sql
+++ subqueries/assignments/assignment_0.sql
@@ -128,3 +128,19 @@ WHERE department_id IN (
         )
     )
 );
+
+-- 9. Write a query to find employee details where the region is 'Asia', 'Europe'
+SELECT * FROM employees
+WHERE department_id IN (
+    SELECT department_id FROM departments
+    WHERE location_id IN (
+        SELECT location_id FROM locations
+        WHERE country_id IN (
+            SELECT country_id FROM countries
+            WHERE region_id IN (
+                SELECT region_id FROM regions
+                WHERE region_name IN ('Asia','Europe')
+            )
+        )
+    )
+);
