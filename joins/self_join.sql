5c46e6b82d216714a980d87df5d457f918d5ff7f
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-10-15 20:58:35 +0530

    feat : added selfjoin.sql with examples

---
+++ joins/self_join.sql
@@ -0,0 +1,19 @@
+-- Display employee names along with their manager names.
+
+-- List employees who have the same manager.
+
+-- Show all managers and the number of employees they manage.
+
+-- Display employees whose salary is higher than their manager’s salary.
+
+-- List employees who joined before their manager.
+
+-- Show all employees along with their manager’s job ID.
+
+-- Display the manager-employee pairs working in the same department.
+
+-- List all employees who don’t have a manager (top-level managers).
+
+-- Find employees who earn more than their manager.
+
+-- Show employees whose job title matches their manager’s job title.
